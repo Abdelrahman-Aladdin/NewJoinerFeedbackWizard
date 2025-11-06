a0c753aa779f6392a390d3d650a08de786d75742
Abdelrahman <abdulrahmanaladdin1@gmail.com>
2025-11-06 16:00:09 +0200

    commit

--- src/NewJoinerFeedbackWizard.Blazor.Client/Pages/Index.razor.cs
+++ src/NewJoinerFeedbackWizard.Blazor.Client/Pages/Index.razor.cs
@@ -1,6 +1,17 @@
-﻿namespace NewJoinerFeedbackWizard.Blazor.Client.Pages;
+﻿using Microsoft.AspNetCore.Components;
+using NewJoinerFeedbackWizard.Dtos.User;
+using NewJoinerFeedbackWizard.Interfaces;
+
+namespace NewJoinerFeedbackWizard.Blazor.Client.Pages;
 
 public partial class Index
 {
+    [Inject]
+    private IUserAppService _userAppService { get; set; } = default!;
+    UserDto? CurrentUser { get; set; }
 
+    protected override void OnInitialized()
+    {
+        CurrentUser = _userAppService.GetCurrentUser();
+    }
 }
